078c93faaf7b85f6f0d96f63d539cc2926cc0430
Sergei Leshchenko <leshchenkoemail@gmail.com>
2022-11-14 10:38:32 +0300

    second commit

--- index.sql
+++ index.sql
@@ -2,3 +2,5 @@ create index name_index on agents using hash(name);
 create index weight_index on agents using btree(weight);
 create index name_artifact on artifact using hash(name);
 create index weight_artifact on artifact using btree(weight);
+create index name_regents on regents using hash(name);
+create index technical_name on regents using hash(name);
